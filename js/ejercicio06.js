ca8e3531df4bd0cc5d0f59fd24ce32e320f3b1fb
Agustina Hernandez <lic.agustinahernandez@gmail.com>
2025-04-22 23:39:22 -0300

    se realizo el ejercicio numero seis

---
+++ js/ejercicio06.js
@@ -0,0 +1,17 @@
+/*6- Realiza un script que escriba una pirámide del 1 al 30 de la siguiente forma :
+
+1
+22
+333
+4444
+55555
+666666
+*/
+
+for(let i=1; i<=30; i++){
+  for(let caracteres=0; caracteres<i; caracteres++){
+    document.writeln(i);
+  }
+  document.writeln('<br>')
+}
+
