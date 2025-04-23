b0061c81263cdca57ac0c4ec568692af5f9a29c3
Agustina Hernandez <lic.agustinahernandez@gmail.com>
2025-04-23 17:31:43 -0300

    resolvi el ejercicio 17

--- js/ejercicio17.js
+++ js/ejercicio17.js
@@ -8,3 +8,20 @@ Input: Hola mundo
 
 Output: la vocal ‘o’ está en la posición 1
 */
+
+let texto = prompt("Introduce un texto:");
+
+const vocales = "aeiouáéíóúAEIOUÁÉÍÓÚ";
+
+let encontrada = false;
+for (let i = 0; i < texto.length; i++) {
+    if (vocales.includes(texto[i])) {
+        alert(`La vocal '${texto[i]}' está en la posición ${i + 1}`);
+        encontrada = true;
+        break;
+    }
+}
+
+if (!encontrada) {
+    alert("No se encontró ninguna vocal en el texto.");
+}
