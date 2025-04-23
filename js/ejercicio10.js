c5e75d812dfc4631b6f64c19da7aa756d3e2914b
Agustina Hernandez <lic.agustinahernandez@gmail.com>
2025-04-23 17:50:47 -0300

    realice el ejercicio numero 10

--- js/ejercicio10.js
+++ js/ejercicio10.js
@@ -2,3 +2,16 @@
 Dentro de cada una de las celdas deberá escribirse un número consecutivo en orden descendente. 
 Si, por ejemplo, la tabla es de 7×5 los números irán del 35 al 1.
 */
+
+const filas = parseInt(prompt("Ingresa un numero de filas"))
+const columnas = parseInt(prompt("Ingresa un numero de columnas"))
+document.writeln("<table><tbody>");
+
+for(let indiceFilas =0; indiceFilas < filas; indiceFilas++){
+    document.writeln("<tr>")
+    for(let indiceColumnas=0; indiceColumnas< columnas; indiceColumnas++){
+        document.writeln("<td>35</td>")
+    }
+    document.writeln("</tr>")
+}
+document.writeln("</table></tbody>")
