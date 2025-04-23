3ff798af2cc1ee43eaf1926fc4fa4c3474a41e9b
Agustina Hernandez <lic.agustinahernandez@gmail.com>
2025-04-23 17:56:25 -0300

    realice el ejercicio 13

--- js/ejercicio13.js
+++ js/ejercicio13.js
@@ -1,1 +1,5 @@
 //13- Realiza un script que pida un texto y lo muestre en mayúsculas.
+
+let texto = prompt("Introduce un texto:");
+let mayusculas = texto.toUpperCase();
+document.writeln("El texto en mayúsculas es: " + mayusculas);
