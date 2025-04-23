850c038c964fc4e93f7f68238725ee4997e9f6d1
Agustina Hernandez <lic.agustinahernandez@gmail.com>
2025-04-23 00:01:20 -0300

    realice el ejercicio doce

---
+++ js/ejercicio12.js
@@ -0,0 +1,4 @@
+//12- Realiza un script que genere un número aleatorio entre 1 y 99
+
+let numeroAleatorio = Math.floor(Math.random() * 99) + 1;
+document.writeln("Número aleatorio entre 1 y 99: " + numeroAleatorio);
